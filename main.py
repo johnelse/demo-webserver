fcc5112ba933cac3b1194a301d819e681395c66f
John Else <john.else@citrix.com>
2014-08-18 12:10:55 +0100

    Add comments
    Signed-off-by: John Else <john.else@citrix.com>

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from bottle import route, run, static_file, view
 
 visitors = 0
 
+""" Turn the visitor count into a nice string (1st, 2nd, 3rd etc. ). """
 def get_visitor_index(visitors):
     suffix = ""
     if visitors in [11, 12, 13]:
@@ -20,6 +21,7 @@ def get_visitor_index(visitors):
             suffix = "th"
     return "%d%s" % (visitors, suffix)
 
+""" Root page with visitor counter. """
 @route('/')
 @view('root')
 def root():
@@ -28,6 +30,7 @@ def root():
     visitor_index = get_visitor_index(visitors)
     return dict(visitor_index = visitor_index)
 
+""" Serve static files. """
 @route('/static/<filepath:path>')
 def serve_static(filepath):
     return static_file(filepath, root='static')
